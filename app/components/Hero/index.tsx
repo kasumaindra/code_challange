be8c59f779456703ff58c94e702b2f5dca3cd2da
kasumaindra <kasumaindra36@gmail.com>
2024-05-27 23:14:38 +0700

    update :add hero

--- app/components/Hero/index.tsx
+++ app/components/Hero/index.tsx
@@ -1,5 +1,25 @@
+import Image from 'next/image';
 import React from 'react';
+import hero from '@/public/portrait-beautiful-woman-with-clear-skin-posing-with-leaf-copy-space 1.png';
 
 export const Hero = () => {
-  return <div className="">Hero</div>;
+  return (
+    <div className="h-screen w-screen relative overflow-hidden">
+      <Image className="w-full h-full object-cover" src={hero} alt="hero" />
+      <div className="absolute inset-y-0 left-0 flex flex-col justify-center pl-20">
+        <div>
+          <h1 className="text-gray-600 text-6xl font-bold text-left italic">HIDUP LEBIH SEHAT</h1>
+          <h1 className="text-gray-600 text-6xl font-bold text-left italic">DAN SEJAHTERA</h1>
+          <hr className="border-b-2 border-gray-600 w-150 mt-2" />
+        </div>
+        <div className="py-10">
+          <h1 className="text-white text-4xl font-semibold text-left">Mulailah hari anda dengan</h1>
+          <h1 className="text-white text-4xl font-semibold text-left">perawatan yang sehat dan alami</h1>
+        </div>
+        <div className="pt-32">
+          <button className="rounded-full bg-pink-500 py-3 px-8 text-white text-2xl">SELENGKAPNYA</button>
+        </div>
+      </div>
+    </div>
+  );
 };
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,5 +1,42 @@
+import Image from 'next/image';
 import React from 'react';
+import logo from '@/public/logo.png';
 
 export const Navbar = () => {
-  return <div>Navbar New New New Baruuu</div>;
+  return (
+    <nav className="bg-gray-600 p-8 w-screen">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-lg font-bold">
+          <Image src={logo} alt="logo" width={120} height={90} />
+        </div>
+        <ul className="flex space-x-4 items-center">
+          <li>
+            <a href="#" className="text-white hover:text-gray-300">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-white hover:text-gray-300">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-white hover:text-gray-300">
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-white hover:text-gray-300">
+              Contact
+            </a>
+          </li>
+          <li>
+            <button className="rounded-xl bg-white p-3">Login</button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
+
+export default Navbar;
